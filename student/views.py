583a290ce9ad19f15286d10116a92abb9ea06394
Sitha Lek <sitha.lek@gmail.com>
2025-12-22 11:51:01 +0700

    extend layout

--- student/views.py
+++ student/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 def home(request):
-    return HttpResponse("my Home")
-    # return render(request,'home.html')
+    # return HttpResponse("my Home")
+    return render(request,'home.html')
 def student(request):
     return render(request,'index.html')
