659687d78ecff28cc7527b62687a730368fdfe1a
unknown <sitha.lek@gmail.com>
2025-12-22 10:45:03 +0700

    edit home

--- student/views.py
+++ student/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 def home(request):
     return HttpResponse("my Home")
+    # return render(request,'home.html')
 def student(request):
     return render(request,'index.html')
