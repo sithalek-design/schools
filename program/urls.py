b4765974b6e0e0d0d01ab86d6055d6d0254112a5
Sitha Lek <sitha.lek@gmail.com>
2025-12-31 07:08:18 +0700

    save mainprogram

--- program/urls.py
+++ program/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
     
     path('main-program/',view=views.mainprogram,name='main-program'),
+    path('main-program-save/',view=views.mainprogram_save,name='main-program-save'),
     path('main-program-data/',view=views.mainprogram_data,name='main-program-data'),
     path('sub-program/',view=views.subprogram,name='sub-program'),
     
--- program/views.py
+++ program/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import *
+from django.forms.models import model_to_dict
 
 def mainprogram(request):
     mainprogramform=MainProgramForm()
@@ -8,14 +9,15 @@ def mainprogram(request):
         'mainprogramform':mainprogramform,
     }
     return render(request,'mainprogram.html',context)
-def mainprogram_data(request):
+def mainprogram_save(request):
     if request.method=='POST':
-        mpprogramkh=request.POST['mpprogramkh']
-        mpprogramen=request.POST['mpprogramkh']
-        abbreviation=request.POST['abbreviation']
-        mainprogram=MainProgram(mp_name_kh=mpprogramkh,mp_name_en=mpprogramen,abbreviation=abbreviation)
-        mainprogram.save()
-        return HttpResponse("Save done!")
+        # print(request.POST)
+        mainprogram=MainProgramForm(request.POST)
+        mp=mainprogram.save()
+        return JsonResponse(model_to_dict(mp),safe=False)
+def mainprogram_data(request):
+    mainprogram=MainProgram.objects.all()
+    return JsonResponse({"mainprogram":list(mainprogram.values())})
 
 def subprogram(request):
     return render(request,'subprogram.html')
