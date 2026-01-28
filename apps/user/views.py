f96cf282709a1886cc14bd8aa6d020aa34698a75
Sitha Lek <sitha.lek@gmail.com>
2026-01-28 07:10:32 +0700

    doing login

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 
 def registration(request):
-    form_user=UserChangeForm()
+    form_user=UserCreationForm()
+    if request.method=='POST':
+        form_user=UserCreationForm(request.POST) 
+        if form_user.valid():
+            form_user.save()
+    
+
     context={
 
         'form_user':form_user
     }
 
-    return render(request,'user/index.html',context)
+    return render(request,'user.html',context)
 
 # Create your views here.
